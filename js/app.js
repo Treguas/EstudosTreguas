4b0a173febbba0c8fa925ae7c957bc293b530ffc
Vagner Treguas <vagner.treguas@gmail.com>
2020-07-30 02:33:55 -0300

    Function 2 Valores

--- js/app.js
+++ js/app.js
@@ -4,3 +4,19 @@ var soma = n1 + n2
 
 window.alert ('A soma dos valores é: '+ soma);
 
+
+//Função, Dois tipos de Função Realiza uma tarefa, não devolve valor
+function dizerNome() {
+  console.log('Spaceborne');
+}
+//Funçao, que retorna um valor;
+function aluno(nota){
+  if (nota <= 5){
+    return 'reprovado'
+  } else {
+    return 'aprovado'
+  }
+}
+
+const resultado = aluno(10)
+console.log(resultado);
