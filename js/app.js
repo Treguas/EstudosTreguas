2bc7e35a37b29653a5b36c50ece2e38c86458a39
Treguas <vagner.treguas@gmail.com>
2020-09-07 13:55:31 -0300

    JSON

--- js/app.js
+++ js/app.js
@@ -14,3 +14,16 @@ title.addEventListener('click', mudarTexto=>{
     title.innerHTML = 'mudoudenovo'
 })
 
+
+var elementos = document.querySelectorAll('p');
+
+/* -- Exemple one.
+for (var i = 0; i < elementos.length; i++) {
+    elementos[i].style.background ="gold";
+}*/
+
+// exemplo2
+for (const item of elementos) {
+    item.style.background = "yellow";
+    item.style.textAlign="center"
+    }
