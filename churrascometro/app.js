1c7ce18d6b3a72c7f345da3be2e7adada236008a
Treguas <vagner.treguas@gmail.com>
2020-09-08 17:53:03 -0300

    churrascometro

---
+++ churrascometro/app.js
@@ -0,0 +1,51 @@
+let inputAdultos = document.getElementById('adultos');
+let inputCriancas = document.getElementById('criancas');
+let inputDuracao = document.getElementById('duracao');
+let resultado = document.getElementById('resultado');
+
+
+
+function calcular() {
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qdtTotalCarnes = carnesPP(duracao) * adultos + (carnesPP(duracao) / 2 * criancas);
+    let qdtTotalCervejas = cervejasPP(duracao) * adultos;
+    let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
+
+
+    
+    resultado.innerHTML = `<p>${qdtTotalCarnes}g de Carnes</p>`
+    resultado.innerHTML += `<p>${qdtTotalCervejas}ml de Cervejas</p>`
+    resultado.innerHTML += `<p>${qdtTotalBebidas}ml de Bebidas</p>`
+      
+}
+
+
+function carnesPP(duracao) {
+    if(duracao >= 6) {
+        return 650;
+    }else {
+        return 400;
+    }
+}
+
+
+function cervejasPP(duracao) {
+    if(duracao >= 6) {
+        return 2000;
+    }else {
+        return 1200;
+    }
+}
+
+
+function bebidasPP(duracao) {
+    if(duracao >= 6) {
+        return 1500;
+    }else {
+        return 1000;
+    }
+}
