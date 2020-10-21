702f1f474527cee2585998d0e8b0405ca8d9bf5d
Vagner Treguas <vagner.treguas@gmail.com>
2020-10-21 12:41:50 -0300

    estudos javascript

---
+++ JavaScript/arrays.js
@@ -0,0 +1,11 @@
+const alunos = ['luiz', 'Maria', 'Julimar', 'Hailie'];
+
+
+
+
+alunos.push('Jessica');// adiciona no final do array
+alunos.unshift('Vagner'); //Adiciona elemento no começo do array
+alunos.shift(); //Remove do começo
+alunos.pop(); // Remove elemento do final
+
+console.log(alunos);
