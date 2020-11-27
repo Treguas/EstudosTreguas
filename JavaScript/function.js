8207df12f7a62b3f9e02319ed0defbeeacb23f40
Vagner Treguas <vagner.treguas@gmail.com>
2020-11-27 17:37:09 -0300

    Functions

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -69,3 +69,19 @@ function conta(operador, acumulador, ...numeros) {
 }
 
 conta('+', 2, 1,20,50,30);
+
+// RETURN = Retorna um valor =)
+function criaMultiplicador(multiplicador) {
+    //multiplicador
+    return function(n) {
+        return n * multiplicador;
+    };
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica =  criaMultiplicador(4);
+
+console.log(duplica(3));
+console.log(triplica(3));
+console.log(quadriplica(3));
