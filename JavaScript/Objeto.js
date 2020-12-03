8c94da8962b0c22189424284a9a49820d47a8f9d
Vagner Treguas <vagner.treguas@gmail.com>
2020-12-03 18:42:39 -0300

    Obj

--- JavaScript/Objeto.js
+++ JavaScript/Objeto.js
@@ -4,7 +4,13 @@ const pessoa = {
     idade: 30,
     fala() {
         console.log(`Hello my name is ${this.nome} ${this.sobrenome}`);
+    },
+    getDataNascimento() {
+        const dataAtual =  new Date();
+        return dataAtual.getFullYear() - this.idade;
     }
 };
 
 pessoa.fala();
+console.log(pessoa['idade']);
+console.log(pessoa.getDataNascimento());
