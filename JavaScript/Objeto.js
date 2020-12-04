c8a78a12727f6e635c3ed0aa2a9af28d5db499df
Vagner Treguas <vagner.treguas@gmail.com>
2020-12-04 14:10:55 -0300

    Objetos com função construtora

--- JavaScript/Objeto.js
+++ JavaScript/Objeto.js
@@ -3,7 +3,7 @@ const pessoa = {
     sobrenome: 'Treguas',
     idade: 30,
     fala() {
-        console.log(`Hello my name is ${this.nome} ${this.sobrenome}`);
+        console.log(`Hello my name is ${this.nome} ${this.sobrenome} I was born in the year ${this.getDataNascimento()}`);
     },
     getDataNascimento() {
         const dataAtual =  new Date();
@@ -14,3 +14,20 @@ const pessoa = {
 pessoa.fala();
 console.log(pessoa['idade']);
 console.log(pessoa.getDataNascimento());
+
+
+function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, //Mostra a chave
+        value: estoque, //Valor
+        writable: false, // Pode alterar ou não
+        configurable: true // Permite reconfigurar as chaves
+    });
+}
+
+const p1 = new Produto('Camiseta', 20, 3);
+const p2 = new Produto('Computador', 999, 4);
+console.log(p1, p2);
+console.log(p1.nome, p1.preco, p2.nome, p2.preco);
